61b188fcbfcd4cec8cda514219719a8ec4cc4866
jimmy <jhtokd@gmail.com>
2019-11-24 21:31:43 +0800

    feat: use articleEditor to handle the fetching data

--- src/js/components/contents/ArticleEdit/ArticleEdit.js
+++ src/js/components/contents/ArticleEdit/ArticleEdit.js
@@ -2,12 +2,13 @@ import React, {useEffect} from 'react';
 import { useParams, useHistory } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchArticle, updateArticle } from 'Store/articles/articlesAction';
+import ArticleEditor from 'Components/common/ArticleEditor/ArticleEditor';
 
 const ArticleEdit = () => {
+	const history = useHistory();
 	const { id } = useParams();
 	const dispatch = useDispatch();
 	const article = useSelector(state => {
-		console.log('state.articlesReducer.article:  ', state.articlesReducer.article);
 		return state.articlesReducer.article
 	});
 	const getArticle = () => {
@@ -18,9 +19,25 @@ const ArticleEdit = () => {
 		getArticle();
 	},[]);
 
+
+	const submit = (article) => {
+		dispatch(updateArticle(id, article));
+	}
+
+	const cancel = () => {
+		const answer = window.confirm("確定放棄修改？")
+		if (answer) {
+			history.replace('/');
+		}
+	}
+
 	return (
 		<div>
-			ArticleEdit
+			{ article && article.id === id ? (
+				<ArticleEditor defaultArticle={article} submit={submit} cancel={cancel}/>
+			):(
+				<div> loading... </div>
+			)}
 		</div>
 	);
 };
