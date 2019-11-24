fc8dd80c995f7b2765b7251fbc907d062407446e
jimmy <jhtokd@gmail.com>
2019-11-24 22:02:38 +0800

    feat: add gitlab url

--- src/js/components/common/partials/Header.js
+++ src/js/components/common/partials/Header.js
@@ -10,7 +10,7 @@ const Header = () => {
 					<img className="logo cursor-pointer" src={Logo} alt="logo"/>
 				</Link>
 				<div className="col-5 col-lg-3 d-flex justify-content-end align-items-center">
-					<a className="col-6 text-white d-flex align-items-center no-underline" href="/">
+					<a className="col-6 text-white d-flex align-items-center no-underline" href="https://gitlab.com/jhtokd/markdown-article-cms">
 						<i className="fa-lg fab fa-gitlab" />
 						<span className="ml-2 d-none d-sm-block">程式碼</span>
 					</a>
